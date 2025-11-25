afcf679f2a7dd28feeaa129add23665f31bdb9ed
Swiss Cheese <jjrowlaand@gmail.com>
2025-11-25 16:31:38 +1300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use sdl2::event::Event;
 use sdl2::render::{WindowCanvas, Texture, Canvas};
 use sdl2::rect::{Point, Rect};
 use sdl2::image::LoadTexture;
-use sdl2::mouse::MouseState;
+use sdl2::mouse::{MouseState, MouseWheelDirection};
 use sdl2::video::Window;
 use sdl2::surface::Surface;
 
@@ -29,6 +29,7 @@ fn mouse_x(canvas: &mut WindowCanvas, e: &sdl2::EventPump) -> i32 {
     let (width, _height) = canvas.output_size().unwrap();
     let mouse_state: MouseState = e.mouse_state();
     let x = mouse_state.x();
+    println!("X: {}", x - width as i32 / 2);
     return x - width as i32 / 2;
 }
 fn mouse_x_abs(e: &sdl2::EventPump) -> i32 {
@@ -40,6 +41,7 @@ fn mouse_y(canvas: &mut WindowCanvas, e: &sdl2::EventPump) -> i32 {
     let (_width, height) = canvas.output_size().unwrap();
     let mouse_state: MouseState = e.mouse_state();
     let y = mouse_state.y();
+    println!("Y: {}", y - height as i32 / 2);
     return y - height as i32 / 2;
 }
 fn mouse_y_abs(e: &sdl2::EventPump) -> i32 {
@@ -55,6 +57,12 @@ fn pressed(x: i32, y: i32, width: i32, height: i32, canvas: &mut WindowCanvas, e
     }
 }
 fn pressed_abs(x: i32, y: i32, width: i32, height: i32, e: &sdl2::EventPump) -> bool {
+    if e.mouse_state().left() {
+        println!("X > x - (w / 2): {}", mouse_x_abs(e) > x - (width / 2));
+        println!("X < w: {}", mouse_x_abs(e) < width);
+        println!("Y > y - (h / 2): {}", mouse_y_abs(e) > y - (height / 2));
+        println!("Y < h: {}" , mouse_y_abs(e) < height);
+    }
     if e.mouse_state().left() && mouse_x_abs(e) > x - (width / 2) && mouse_x_abs(e) < width && mouse_y_abs(e) > y - (height / 2) && mouse_y_abs(e) < height {
         return true;
     } else {
@@ -73,20 +81,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build()
         .expect("failed to build window's canvas");
     let texture_creator = canvas.texture_creator();
+    let ttf_context = sdl2::ttf::init()?;
     // surface data
     let mut surface = Surface::new(1600, 800, PixelFormatEnum::RGBA32)?;
     // texture data
     let exe_dir = current_exe()?.parent().expect("").display().to_string();
     let mut textures = vec![texture_creator.load_texture(format!("{}/mica_assets/menu/logo.png", exe_dir)).expect(""), texture_creator.load_texture(format!("{}/mica_assets/menu/image.png", exe_dir)).expect("")];
     let mut positions: Vec<Point>;
+    let mut sliders: Vec<Point> = vec![];
     let mut sprites = vec![Rect::new(0, 0, 500, 166), Rect::new(0, 0, 300, 100)];
+    // brush data
     let mut lastdrawnx = -1;
     let mut lastdrawny = -1;
+    let mut size = 1;
     // other data
     let mut room = "menu";
     let mut event_pump = sdl_context.event_pump()?;
     let mut selected = 0;
     let mut loaded_textures = false;
+    // font data
+    let font = ttf_context.load_font(format!("{}/mica_assets/verdana.ttf", exe_dir), 16)?;
     // enter loop
     'running: loop {
         // handle events
@@ -94,6 +108,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             match event {
                 Event::Quit {..} => {
                     break 'running;
+                },
+                Event::MouseWheel { y, direction, .. } => {
+                    match direction {
+                        MouseWheelDirection::Normal => {
+                            if y > 0 {
+                                size += 1;
+                            } else if y < 0 {
+                                size -= 1;
+                            }
+                        },
+                        MouseWheelDirection::Flipped => {
+                            if y > 0 {
+                                size += 1;
+                            } else if y < 0 {
+                                size -= 1;
+                            }
+                        },
+                        _ => {}
+                    }
                 },
                 _ => {}
             }
@@ -109,24 +142,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     texture_creator.load_texture(format!("{}/mica_assets/image/menubar.png", exe_dir))?,
                     texture_creator.load_texture(format!("{}/mica_assets/image/paint.png", exe_dir))?,
                     texture_creator.load_texture(format!("{}/mica_assets/image/erase.png", exe_dir))?,
+                    texture_creator.load_texture(format!("{}/mica_assets/oneshot.png", exe_dir))?,
+                    texture_creator.load_texture(format!("{}/mica_assets/oneshot.png", exe_dir))?,
+                    texture_creator.load_texture(format!("{}/mica_assets/oneshot.png", exe_dir))?,
                 ];
                 sprites = vec![
                     Rect::new(0, 0, 1632, 916),
+                    Rect::new(0, 0, 32, 32),
+                    Rect::new(0, 0, 32, 32),
+                    Rect::new(0, 0, 32, 32),
                     Rect::new(0, 0, 32, 32),
                     Rect::new(0, 0, 32, 32),
                 ];
                 loaded_textures = true;
             }
             positions = vec![
-                Point::new(width_abs + 801, height_abs + 442),
+                Point::new(width_abs + 832, height_abs + 342),
                 Point::new(width_abs, height_abs),
                 Point::new(width_abs, height_abs + 32),
+                Point::new(width_abs, height_abs + 64),
+                Point::new(width_abs, height_abs + 64),
+                Point::new(width_abs, height_abs + 64),
             ];
-            if pressed_abs(0, 0, 32, 32, &event_pump) {
-                selected = 1;
-            }
-            if pressed_abs(0, 64, 32, 64, &event_pump) {
-                selected = 2;
+            if event_pump.mouse_state().left() {
+                if pressed_abs(0, 0, 32, 32, &event_pump) {
+                    selected = 1;
+                }
+                if pressed_abs(0, 64, 32, 64, &event_pump) {
+                    selected = 2;
+                }
+                if pressed_abs(45, 835, 300, 25, &event_pump) {
+                    selected = 3;
+                }
+                if pressed_abs(45, 860, 300, 25, &event_pump) {
+                    selected = 4;
+                }
+                if pressed_abs(45, 885, 300, 25, &event_pump) {
+                    selected = 5;
+                }
+                println!("X: {}, Y: {}", mouse_x_abs(&event_pump), mouse_y_abs(&event_pump));
+            } else if selected == 3 || selected == 4 || selected == 5 {
+                selected = 0;
             }
             if selected != 0 && event_pump.mouse_state().left() {
                 if selected == 1 {
@@ -140,13 +196,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         for _ in 0..1600 {
                             x += xstep;
                             y += ystep;
-                            let surfrect = Rect::from_center(Point::new(x as i32, y as i32), 16, 16);
+                            let surfrect = Rect::from_center(Point::new(x as i32, y as i32), (1 * size) as u32, (1 * size) as u32);
                             surface.fill_rect(Some(surfrect), Color::RGBA(0, 0, 0, 255))?;
                         }
                         lastdrawnx = mouse_x_abs(&event_pump) - 32;
                         lastdrawny = mouse_y_abs(&event_pump);
                     } else {
-                        let surfrect = Rect::from_center(Point::new(mouse_x_abs(&event_pump) - 32, mouse_y_abs(&event_pump)), 16, 16);
+                        let surfrect = Rect::from_center(Point::new(mouse_x_abs(&event_pump) - 32, mouse_y_abs(&event_pump)), (1 * size) as u32, (1 * size) as u32);
                         surface.fill_rect(Some(surfrect), Color::RGBA(0, 0, 0, 255))?;
                         lastdrawnx = mouse_x_abs(&event_pump) - 32;
                         lastdrawny = mouse_y_abs(&event_pump);
@@ -163,17 +219,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         for _ in 0..1600             {
                             x += xstep;
                             y += ystep;
-                            let surfrect = Rect::from_center(Point::new(x as i32, y as i32), 16, 16);
+                            let surfrect = Rect::from_center(Point::new(x as i32, y as i32), (1 * size) as u32, (1 * size) as u32);
                             surface.fill_rect(Some(surfrect), Color::RGBA(0, 0, 0, 0))?;
                         }
                         lastdrawnx = mouse_x_abs(&event_pump) - 32;
                         lastdrawny = mouse_y_abs(&event_pump);
                     } else {
-                        let surfrect = Rect::from_center(Point::new(mouse_x_abs(&event_pump) - 32, mouse_y_abs(&event_pump)), 16, 16);
+                        let surfrect = Rect::from_center(Point::new(mouse_x_abs(&event_pump) - 32, mouse_y_abs(&event_pump)), (1 * size) as u32, (1 * size) as u32);
                         surface.fill_rect(Some(surfrect), Color::RGBA(0, 0, 0, 0))?;
                         lastdrawnx = mouse_x_abs(&event_pump) - 32;
                         lastdrawny = mouse_y_abs(&event_pump);
                     }
+                }
+                if selected == 3 || selected == 4 || selected == 5 {
+                    sliders[selected - 3] = Point::new(mouse_x(&mut canvas, &event_pump), 342 + (25 * (selected as i32 - 3)));
                 }
             } else {
                 lastdrawnx = -1;
@@ -182,8 +241,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         } else if pressed(0, 0, 300, 100, &mut canvas, &event_pump) {
             room = "image";
         }
+        // font surface
+        let fontsurf = font.render(&format!("Brush Size: {}", size))
+            .blended(Color::RGBA(0, 0, 0, 255))?;
+        let fonttexture = fontsurf.as_texture(&texture_creator)?;
         let texture = surface.as_texture(&texture_creator)?;
+        // render it
         canvas.copy(&texture, None, Some(Rect::new(32, 0, 1600, 800)))?;
+        canvas.copy(&fonttexture, None, Some(Rect::new(0, height as i32 - 32, 112, 32)))?;
         render(&mut canvas, Color::RGBA(255, 255, 255, 255), &textures, positions.clone(), sprites.clone())?;
         canvas.clear();
         // time management
